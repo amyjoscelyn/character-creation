9fa18227bb54126476bf1f3ae0b12e2a99c0695c
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-10-21 13:38:45 -0400

    initial commit

---
+++ CharacterCreation/CharacterCreation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CharacterCreation
+//
+//  Created by Amy Joscelyn on 10/21/15.
+//  Copyright © 2015 Amy Joscelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CharacterCreation/CharacterCreation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CharacterCreation
+//
+//  Created by Amy Joscelyn on 10/21/15.
+//  Copyright © 2015 Amy Joscelyn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
